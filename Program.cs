64b1e2ec73dff56d4b8260d082baa0e7f0383faa
Thanu <thanutheiven@gmail.com>
2026-07-17 20:43:20 +0200

    Add Docker support and clean ignored files

--- Program.cs
+++ Program.cs
@@ -4,19 +4,20 @@ using projetLocation.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container
 builder.Services.AddControllersWithViews();
 
-// SQLite + DbContext
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddHealthChecks();
 
-// 👉 SERVICES (architecture en couches)
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(
+        builder.Configuration.GetConnectionString("DefaultConnection")
+    )
+);
+
 builder.Services.AddScoped<ReservationService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
@@ -30,9 +31,11 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-// Route principale MVC
+app.MapHealthChecks("/health");
+
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}"
+);
 
 app.Run();
